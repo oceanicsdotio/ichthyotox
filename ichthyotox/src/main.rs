633c71354bd95f1621e70de7a88cbff4bdcb666e
Nicholas Keeney <nkeeney@oceanics.io>
2021-05-12 21:10:21 -0400

    nest

---
+++ ichthyotox/src/main.rs
@@ -0,0 +1,23 @@
+
+mod cyanobacteria;
+mod behavior;
+mod variables;
+mod simulation;
+
+
+
+fn main() {
+
+
+
+    let experiment = Experiment::new();
+
+    // skip creating mesh
+
+
+    // diffusivity, temperature, rho_pure, density
+
+
+
+    println!("Hello, world!");
+}
